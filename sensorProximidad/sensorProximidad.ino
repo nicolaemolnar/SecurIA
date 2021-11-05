3b5ecb03ac7da13329c31bb2b988d19a67600770
Ekk001 <73540438+Ekk001@users.noreply.github.com>
2021-11-05 16:19:25 +0100

    sensorProximidad
    Código Arduino para medir la distancia a un objeto

---
+++ sensorProximidad/sensorProximidad.ino
@@ -0,0 +1,32 @@
+
+int txUS = 8;
+int rxUS = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(txUS, OUTPUT);
+  pinMode(rxUS, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int dist;
+
+  dist = distance(txUS,rxUS);
+  Serial.println(dist);
+  Serial.println(" cm.");
+}
+
+int distance(int TriggerPin, int EchoPin){
+  long duration;
+
+  digitalWrite(TriggerPin, LOW);
+  delayMicroseconds(4);
+  digitalWrite(TriggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(EchoPin, LOW);
+
+  duration = pulseIn(EchoPin, HIGH);
+  return duration*10/292/2;
+}
