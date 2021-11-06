17ba4e4124dd0b5da2cef64e954219be6700963f
AlbertoStvz <albertoestevezc@outlook.com>
2021-11-06 14:03:05 +0100

    Revert "Update MainActivity.java"
    This reverts commit 16002ef18323af6522f3483486a001939ae9151a.

--- SecurIA/app/src/main/java/com/privatecomms/securia/MainActivity.java
+++ SecurIA/app/src/main/java/com/privatecomms/securia/MainActivity.java
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        for(int i;i>0;i++){};
     }
 }
