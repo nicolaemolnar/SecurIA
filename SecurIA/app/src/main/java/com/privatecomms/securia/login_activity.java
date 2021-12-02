7791fed3c34605b4e67ffd02cab1f247aab64d5e
AlbertoStvz <albertoestevezc@outlook.com>
2021-12-02 18:39:06 +0100

    actualización 2

--- SecurIA/app/src/main/java/com/privatecomms/securia/login_activity.java
+++ SecurIA/app/src/main/java/com/privatecomms/securia/login_activity.java
@@ -2,6 +2,7 @@ package com.privatecomms.securia;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,13 +11,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Type;
 import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.Types;
 
 public class login_activity extends Activity {
 
     private TextView ViewEmail, ViewPass;
     private Button btnlogin, btnregister;
     private EditText inputEmailAddress, inputPassword;
+    String urlWeb = "https://SecurIA.com";
 
     //variable de conexion
     private static conexionBD con=new conexionBD();
@@ -42,29 +47,42 @@ public class login_activity extends Activity {
                 Incio_Sesion(inputEmailAddress.getText().toString(),inputPassword.getText().toString());
             }
         });
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ir a la pagina web para realizar el registro
+                Uri link = Uri.parse(urlWeb);
+                Intent i = new Intent(Intent.ACTION_VIEW,link);
+            }
+        });
     }
 
     //Crearemos la Función para Iniciar Sesion de Postgresql
-    public  void Incio_Sesion(String usuario, String clave){
-        try{
-            String storeProcedureCall="{CALL pa_logueo_android(?,?,?,?,?,?,?,?)}";
-            CallableStatement cStmt=con.conexionBD().prepareCall(storeProcedureCall);
+    public  void Incio_Sesion(String usuario, String clave) {
+        try {
+            String storeProcedureCall = "{CALL pa_logueo_android(?,?,?,?,?,?,?,?)}";
+            CallableStatement cStmt = con.conexionBD().prepareCall(storeProcedureCall);
             //Estos dos primeros parametros son los de entrada
-            cStmt.setString(1,usuario);
-            cStmt.setString(2,clave);
-
-
-}
-
-
-
-
+            cStmt.setString(1, usuario);
+            cStmt.setString(2, clave);
+            //Parametros de salida
+            cStmt.registerOutParameter(3, Types.VARCHAR);
+            cStmt.executeUpdate();
+            String msj=cStmt.getString(3); //variable que recibiremos de postgresql
 
 
 
 
-
-
-
-
+            if (msj.equals("OK")){
+                //si el usuario y contraseña correctos entramos en el menu de la aplicación
+                Intent menu = new Intent(this, MainActivity.class);
+                startActivity(menu);
+            }else{
+                Toast.makeText(getApplicationContext(),msj,Toast.LENGTH_SHORT).show();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
--- SecurIA/app/src/main/java/com/privatecomms/securia/main_activity.java
+++
@@ -1,4 +0,0 @@
-package com.privatecomms.securia;
-
-public class main_activity {
-}
