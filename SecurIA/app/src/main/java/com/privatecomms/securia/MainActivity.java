eb5b6053111cca6fe4fc8139dba14899b5d7c034
Eduardouahes <eduardo.garzo@edu.uah.es>
2021-12-03 11:30:34 +0100

    Actualizacion 4
    Creo que ya estarían implementados todos los botones y switches,
    Nota: borrar el save de config y poner ahí un back??

--- SecurIA/app/src/main/java/com/privatecomms/securia/MainActivity.java
+++ SecurIA/app/src/main/java/com/privatecomms/securia/MainActivity.java
@@ -1,8 +1,10 @@
 package com.privatecomms.securia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +17,15 @@ import java.sql.Types;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnConfig, btnExit;
+    SwitchCompat stream;
+    boolean stateStream;
+
+    SharedPreferences preferences;
 
     //variable de conexion
     private static conexionBD con=new conexionBD();
-
     //atributos y sincronizacion
     Atributos_Usuario atrib=Atributos_Usuario.getInstance();
-
     //bundle para rrecbir los datos del login
     Bundle datos = this.getIntent().getExtras();
 
@@ -30,14 +34,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        //Init
+        //Init de botones
         this.btnConfig = this.findViewById(R.id.btnConfig);
         this.btnExit = this.findViewById(R.id.btnExit);
+        //switch de stream
+        preferences = getSharedPreferences("PREFS",0);
+        stateStream = preferences.getBoolean("stream",false);
+        this.stream = this.findViewById(R.id.stream);
 
         //le paso los datos del usuario del inicio de sesion del login
         String email = datos.getString("email");
         String password = datos.getString("password");
 
+        //funciones de botones y switch de stream
         btnConfig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +57,19 @@ public class MainActivity extends AppCompatActivity {
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //poner que salga de la app
+                finish();
+                System.exit(0);
+            }
+        });
+
+        stream.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stateStream = !stateStream;
+                stream.setChecked(stateStream);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("stream",stateStream);
+                editor.apply();
             }
         });
 
@@ -61,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             String storeProcedureCall = "{CALL pa_logueo_android(?,?,?,?,?,?,?,?)}";
             CallableStatement cStmt = con.conexionBD().prepareCall(storeProcedureCall);
 
-            //Estos dos primeros parametros son los de entrada
+            //Estos dos primeros parametros son los de entrada que hemos recibido de login
             cStmt.setString(1, usuario);
             cStmt.setString(2, clave);
             //Parametros de salida
--- SecurIA/app/src/main/java/com/privatecomms/securia/config_activity.java
+++ SecurIA/app/src/main/java/com/privatecomms/securia/config_activity.java
@@ -1,20 +1,120 @@
 package com.privatecomms.securia;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.widget.SwitchCompat;
+
 public class config_activity extends Activity {
+    Atributos_Usuario atrib=Atributos_Usuario.getInstance();
     EditText userName,email, password, repeatedPassword;
+    Button btnBack,btnExit;
+
+    SwitchCompat sendNotifications,captureFotos,lifeStream,captureVideos;
+    boolean stateSwitch1,stateSwitch2,stateSwitch3,stateSwitch4;
+
+    SharedPreferences preferences;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.config_activity);
 
+        //init de los botones
+        this.btnBack = this.findViewById(R.id.btnBack);
+        this.btnExit = this.findViewById(R.id.btnExit);
+
+        //inicio de atributos de usuario y posibilidad de cambio de los mismos
         this.userName = this.findViewById(R.id.userName);
         this.email = this.findViewById(R.id.email);
         this.password = this.findViewById(R.id.password);
         this.repeatedPassword = this.findViewById(R.id.repeatedPassword);
+
+        userName.setText(atrib.get_userName().toString());
+        email.setText(atrib.get_email().toString());
+        password.setText(atrib.get_password().toString());
+        repeatedPassword.setText(atrib.get_repeatedPassword().toString());
+
+
+        //inicio de los botones de switch de las opciones
+        preferences = getSharedPreferences("PREFS",0);
+        stateSwitch1 = preferences.getBoolean("sendNotifications",false);
+        stateSwitch2 = preferences.getBoolean("captureFotos",false);
+        stateSwitch3 = preferences.getBoolean("lifeStream",false);
+        stateSwitch4 = preferences.getBoolean("captureVideos",false);
+
+        this.sendNotifications = this.findViewById(R.id.sendNotifications);
+        this.captureFotos = this.findViewById(R.id.captureFotos);
+        this.lifeStream = this.findViewById(R.id.lifeStream);
+        this.captureVideos = this.findViewById(R.id.captureVideos);
+
+        //funciones de botones
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ir a la pagina web para realizar el registro
+                Intent menu = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(menu);
+            }
+        });
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        //funciones de activacion o desactivacion de los switches
+        sendNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stateSwitch1 = !stateSwitch1;
+                sendNotifications.setChecked(stateSwitch1);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("sendNotifications",stateSwitch1);
+                editor.apply();
+            }
+        });
+
+        captureFotos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stateSwitch2 = !stateSwitch2;
+                captureFotos.setChecked(stateSwitch2);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("captureFotos",stateSwitch2);
+                editor.apply();
+            }
+        });
+
+        lifeStream.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stateSwitch3 = !stateSwitch3;
+                lifeStream.setChecked(stateSwitch3);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("lifeStream",stateSwitch3);
+                editor.apply();
+            }
+        });
+
+        captureVideos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stateSwitch4 = !stateSwitch4;
+                captureVideos.setChecked(stateSwitch4);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("captureVideos",stateSwitch4);
+                editor.apply();
+            }
+        });
 
     }
 
