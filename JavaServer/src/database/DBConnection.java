21bfb6607aafa1e9635ca0a6fcbe842b91ec060e
AlbertoStvz <albertoestevezc@outlook.com>
2021-12-24 18:30:35 +0100

    SevletsContact

--- JavaServer/src/database/DBConnection.java
+++ JavaServer/src/database/DBConnection.java
@@ -89,13 +89,20 @@ public class DBConnection {
 
     public boolean insertContact(String name, String email, String phone, String company, String message){
         // TODO Prepare SQL call
+        Statement csmt = null;
 
+        try {
+            csmt = connection.createStatement();
         // TODO Begin transaction
-
+            beginTransaction(csmt);
         // TODO Insert the contact into the contacts table
-
+            String query = "INSERT INTO public.Contact(name,email,phone,company,message) VALUES (name,email,phone,company,masasage);";
+            csmt.execute(query);  //Donde capturo si no se realiza bien el insert para dev false
         // TODO Commit transaction
-
+            closeTransaction(csmt);
+        }catch (SQLException e) {
+            cancelTransaction(csmt);
+        }
         // TODO Return true if successful, false if not
         return true;
     }
--- JavaServer/src/servlets/doContact.java
+++ JavaServer/src/servlets/doContact.java
@@ -1,26 +1,47 @@
 package servlets;
 
+import database.DBConnection;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class doContact extends HttpServlet {
     public doContact() {
         super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         // TODO implement doGet
         doPost(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Get parameters
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String company = request.getParameter("company");
+        String message = request.getParameter("message");
+        boolean validContact =false;
 
         // TODO Connect to database
+        DBConnection db = new DBConnection("postgres","123456");
+        db.obtainConnection();
 
         // TODO Insert contact into database
+        if (db.isConnected()) {
+            validContact = db.insertContact(name,email,phone,company,message);
+        }
 
         // TODO Redirect to confirmation page if successful
+        if (validContact){
+            response.sendRedirect("/securia/confimationPage");//Pagina de confimacion
+
+        }else{
+            response.sendRedirect("/securia/contact.html");
+        }
     }
 }
